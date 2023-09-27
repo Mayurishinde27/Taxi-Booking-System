617b957006eca580f78eeb31077d20b498d89010
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2023-09-27 17:48:00 +0530

    Update Driver_viewBooking.py

--- Taxi-Booking-System/Driver_viewBooking.py
+++ Taxi-Booking-System/Driver_viewBooking.py
@@ -89,7 +89,7 @@ class DriverVieBookingClass():
         self.show() 
         
     def show(self):
-        con = mysql.connector.connect(host = 'localhost' ,username = 'root', password = 'Richa@3217', database = 'taxi_database')
+        con = mysql.connector.connect(host = 'localhost' ,username = 'root', password = 'xyz', database = 'taxi_database')
         cur=con.cursor()
         try:
             cur.execute("select * from admin_page")
